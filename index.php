3422264bb9ade67af4af31b66be3b5068d5b32f1
clayton <relacionamento@ag29.com.br>
2023-05-29 00:38:50 -0300

    Git push repository

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="_cdn/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="_cdn/css/style.css">
+
+    <title>Grupo Colabore</title>
+</head>
+
+<body>
+    <div class="main_header">
+        <div class="main_header_line">
+            <div class="main_header_line_content content">
+                <p>31 3473-9005</p>
+            </div>
+        </div>
+
+        <div class="main_header_nav">
+            <div class="main_header_nav_content content">
+
+                <div class="topnav">
+                    <div class="main_header_nav_logo">
+                        <a href="#home" class="active">Logo</a>
+                    </div>
+                    <div id="myLinks">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Serviços</a></li>
+                            <li><a href="">Vigilância</a></li>
+                            <li><a href="">Contato</a></li>
+                            <li><a href="">Vagas</a></li>
+                        </ul>
+                    </div>
+
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                        <i class="fa-solid fa-bars"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="main_banner">
+        <div class="main_banner_content content">
+            <div class="main_banner_content_text">
+                <div class="main_banner_content_text_desc">
+                    <h2>Serviços terceirizados e segurança para o seu negócio</h2>
+                    <p>Melhor gestão de profissionais, mais eficiência e tranquilidade!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_about">
+        <div class="main_about_content content">
+            <div class="main_about_content_logo">
+                <h2>Logotipo colabore terceirização</h2>
+            </div>
+            <div class="main_about_content_service">
+                <div class="main_about_content_service_desc">
+                    <span>img</span>
+                    <h3>Conservação, higienização e jardinagem</h3>
+                </div>
+                <div class="main_about_content_service_desc">
+                    <span>img</span>
+                    <h3>Mão de obra administrativa</h3>
+                </div>
+                <div class="main_about_content_service_desc">
+                    <span>img</span>
+                    <h3>Manutenção</h3>
+                </div>
+                <div class="main_about_content_service_desc">
+                    <span>img</span>
+                    <h3>Portaria e controle de acesso</h3>
+                </div>
+            </div>
+
+            <div class="main_about_content_logo">
+                <h2>Logotipo colabore vigilância</h2>
+            </div>
+
+            <div class="main_about_content_service">
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Vigilância patrimonial</h3>
+                </div>
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Segurança em eventos</h3>
+                </div>
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Segurança eletrônica</h3>
+                </div>
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Segurança pessoal</h3>
+                </div>
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Central de monitoramento</h3>
+                </div>
+                <div class="main_about_content_service_desc vigilancia">
+                    <span>img</span>
+                    <h3>Consultoria em segurança</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="main_cta_home">
+        <div class="main_cta_home_content content">
+            <div class="main_cta_home_desc">
+                <h2 class="light">Estamos prontos para atender os mais diversos segmentos.</h2>
+                <h2>Bancos, shoppings, indústrias, hospitais, clínicas, eventos, mineradoras, usinas, escolas, universidades e muito mais!</h2>
+                <button class="btn btn-gold"><a href="#">Confira aqui toda nossa variedade de serviços.</a></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_whatsapp_home">
+        <div class="main_whatsapp_home_content content">
+            <div class="main_whatsapp_home_content_desc">
+                <div class="main_whatsapp_home_content_desc_icon">
+                    <span><a href=""><i class="fa-brands fa-whatsapp"></i></a></span>
+                </div>
+                <div class="main_whatsapp_home_content_desc_text">
+                    <h3>Como podemos te ajudar?</h3>
+                    <h2>Faça agora um orçamento!</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_video_home">
+        <div class="main_video_home_content content">
+            <div class="main_video_home_content_desc">
+                <div class="main_video_home_content_desc_left">
+                    <img src="https://placehold.co/600x300/png" alt="Logotipo grupo colabore" />
+                    <p>Oferecemos soluções em serviços terceirizados e segurança para o seu negócio, potencializando o desempenho de suas atividades.</p>
+                </div>
+                <div class="main_video_home_content_desc_right">
+                    <img src="https://placehold.co/300x450/png" alt="Logotipo grupo colabore" />
+                </div>
+            </div>
+            <div class="main_video_home_content_video">
+                <video src="https://placehold.co/850x420.mp4"></video>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_differentials">
+        <div class="main_differentials_content content">
+            <div class="main_differentials_content_desc">
+                <div class="main_differentials_content_title">
+                    <h2>Diferenciais</h2>
+                </div>
+                <div class="main_differentials_content_desc_services">
+                    <div class="main_differentials_content_desc_services_icon">
+                        <span><i class="fa-solid fa-handshake-angle"></i></span>
+                    </div>
+                    <div class="main_differentials_content_desc_services_text">
+                        <h2>Profissionalismo e confiabilidade</h2>
+                        <p>Profissionais qualificados e contantemente atualizados.</p>
+                    </div>
+                </div>
+                <div class="main_differentials_content_desc_services">
+                    <div class="main_differentials_content_desc_services_icon">
+                        <span>icone</span>
+                    </div>
+                    <div class="main_differentials_content_desc_services_text">
+                        <h2>Conformidade com a lei</h2>
+                        <p>100% de acordo às normas trabalhistas, fiscais e tributárias.</p>
+                    </div>
+                </div>
+                <div class="main_differentials_content_desc_services">
+                    <div class="main_differentials_content_desc_services_icon">
+                        <span>icone</span>
+                    </div>
+                    <div class="main_differentials_content_desc_services_text">
+                        <h2>Tecnologia</h2>
+                        <p>Os mais modernos equipamentos e softwares de gestão e control.</p>
+                    </div>
+                </div>
+                <div class="main_differentials_content_desc_services">
+                    <div class="main_differentials_content_desc_services_icon">
+                        <span>icone</span>
+                    </div>
+                    <div class="main_differentials_content_desc_services_text">
+                        <h2>Próximos de você</h2>
+                        <p>Atendimento humanizado e sempre disponível</p>
+                    </div>
+                </div>
+                <div class="main_differentials_content_desc_services">
+                    <div class="main_differentials_content_desc_services_icon">
+                        <span>icone</span>
+                    </div>
+                    <div class="main_differentials_content_desc_services_text">
+                        <h2>Portfólio diversificado</h2>
+                        <p>Soluções eficientes e customizadas para os mais diversos setores.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_footer">
+        <div class="footer_whatspp">
+            <a href="" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+        </div>
+        <div class="main_footer_content content">
+            <div class="main_footer_content_desc">
+                <div class="main_footer_content_desc_units">
+                    <p><strong>Contato</strong></p>
+                    <p>(31) 3473-9005</p>
+                    <p><strong>Acompanhe nosso dia a dia</strong></p>
+                    <p><a href=""><i class="fa-brands fa-instagram"></i>@grupocolabore</a></p>
+                    <p><a href=""><i class="fa-brands fa-facebook-f"></i>/grupocolabore</a></p>
+                </div>
+                <div class="main_footer_content_desc_units">
+                    <div class="main_footer_content_desc_units_address">
+                        <div class="main_footer_content_desc_units_address_icon">
+                            <span>icone</span>
+                        </div>
+                        <div class="main_footer_content_desc_units_address_icon_desc">
+                            <p>Rua Pamplona, 39 - Bairro Conjunto Lagoa</p>
+                            <p>Belo Horizonte - MG - CEP: 31.365-050</p>
+                        </div>
+                    </div>
+                    <div class="main_footer_content_desc_units_buttons">
+                        <button class="btn-footer"><a href="">Código de ética e conduta</a></button>
+                        <button class="btn-footer-white">
+                            <a href=""><i class="fa-solid fa-arrow-down-to-line"></i>Catálogo de serviços</a></button>
+                    </div>
+                </div>
+                <div class="main_footer_content_desc_units">
+                    <a href=""><img src="" alt="logotipo_grupo_colabore"></a>
+                </div>
+            </div>
+        </div>
+        <div class="main_footer_line_white">
+            <div class="main_footer_content content">
+                <div class="main_footer_line_white_text">
+                    <p>&copy 2023 Grupo Colabore</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myLinks");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }
+    </script>
+</body>
+
+</html>
